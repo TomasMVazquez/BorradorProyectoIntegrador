774d32dbd506b68f4ef4d76d8e46b57d90b41496
Mike Courts <courts.mike@gmail.com>
2018-10-17 10:41:24 -0300

    version 1.8.5 firebase

---
+++ app/src/main/java/com/example/digital/borradorproyectointegrador/MultiLogIn.java
@@ -0,0 +1,37 @@
+package com.example.digital.borradorproyectointegrador;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MultiLogIn extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multi_logins);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+    }
+
+
+
+}
