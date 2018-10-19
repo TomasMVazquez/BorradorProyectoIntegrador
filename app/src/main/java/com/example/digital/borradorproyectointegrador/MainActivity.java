d38302141de9e9302ed5319e86e04e104b1b1f33
Toms <tomas.manuel.vazquez@gmail.com>
2018-10-19 19:03:49 -0300

    version 1.8.9.3

--- app/src/main/java/com/example/digital/borradorproyectointegrador/MainActivity.java
+++ app/src/main/java/com/example/digital/borradorproyectointegrador/MainActivity.java
@@ -133,8 +133,7 @@ public class MainActivity extends AppCompatActivity implements PeliculasFragment
         } else if (id == R.id.nav_listas) {
 
         } else if (id == R.id.nav_share) {
-            Intent intentTrailer = new Intent(MainActivity.this, TrailerActivity.class);
-            startActivity(intentTrailer);
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
