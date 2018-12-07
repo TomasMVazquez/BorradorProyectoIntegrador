1188eeb959da16c8983e4fbe9da82fc0d85d26bc
Toms <tomas.manuel.vazquez@gmail.com>
2018-12-07 15:22:02 -0300

    v 3.4.2 mala mia

--- app/src/main/java/com/example/digital/borradorproyectointegrador/view/Adaptadores/PeliculaAdaptador.java
+++ app/src/main/java/com/example/digital/borradorproyectointegrador/view/Adaptadores/PeliculaAdaptador.java
@@ -97,11 +97,9 @@ public class PeliculaAdaptador extends RecyclerView.Adapter {
                     .into(imagen);
 
             long cantEstr = Math.round((peliculas.getVote_average() * 5) / 10);
-
             
             ratingBar.setRating(cantEstr);
             ratingBar.setNumStars(5);
-
 
         }
 
